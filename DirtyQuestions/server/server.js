8e653f660d03d56d7e5cac26566381338b76d88d
Junoh <junohpark@live.ca>
2016-02-28 00:21:09 -0800

    Done dashboard

--- DirtyQuestions/server/server.js
+++
@@ -1,5 +0,0 @@
-Accounts.onCreateUser(function(options, user) {
-    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
-    user.profile = options.profile;
-	return user;
-});
