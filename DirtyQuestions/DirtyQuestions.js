59a8d052d0ac50f866563aebb1ecab2a7b34447c
Jun Young Bae <t3y7@ugrad.cs.ubc.ca>
2016-02-27 17:11:27 -0800

    router

--- DirtyQuestions/DirtyQuestions.js
+++ DirtyQuestions/DirtyQuestions.js
@@ -1,23 +1,9 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+Router.route('/', function () {
+  this.render('login');
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+Router.route('/createquestion', function () {
+  this.render('createquestion');
+});
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
